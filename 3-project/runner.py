7457875bd711d7a1101c68351456fc3187f217ab
brycezxl <vincentlei@163.com>
2020-07-01 14:55:29 +0800

    3

--- 3-project/runner.py
+++ 3-project/runner.py
@@ -55,8 +55,8 @@ class Runner:
     def _build_model(self):
         # self.model = FC(self.args, self.vocab)
         # self.model = RNN(self.args, self.vocab)
-        # self.model = TextCNN(self.args, self.vocab)
-        self.model = Attention(self.args, self.vocab)
+        self.model = TextCNN(self.args, self.vocab)
+        # self.model = Attention(self.args, self.vocab)
 
         self.model.to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args.lr)

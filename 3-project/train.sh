436cb4a8d19610e84020f247675eaada13a77e31
brycezxl <vincentlei@163.com>
2020-07-01 12:08:56 +0800

    3

--- 3-project/train.sh
+++ 3-project/train.sh
@@ -1,3 +1,13 @@
+python main.py --lr 4e-4 --dropout 0.2 --batch-size 32 --max-words 80 --max-num-epochs 20
+echo ' 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ '
 python main.py --lr 4e-4 --dropout 0.2 --batch-size 64 --max-words 80 --max-num-epochs 20
 echo ' 
  
@@ -8,14 +18,4 @@ echo '
  
  
  '
-python main.py --lr 8e-4 --dropout 0.2 --batch-size 64 --max-words 80 --max-num-epochs 20
-echo ' 
- 
- 
- 
- 
- 
- 
- 
- '
-python main.py --lr 2e-4 --dropout 0.2 --batch-size 64 --max-words 80 --max-num-epochs 20
+python main.py --lr 4e-4 --dropout 0.2 --batch-size 16 --max-words 80 --max-num-epochs 20

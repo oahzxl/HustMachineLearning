25e59a6b82842aa8d9681e8eb70f9342ddde50ef
brycezxl <vincentlei@163.com>
2020-07-01 13:25:11 +0800

    3

--- 3-project/runner.py
+++ 3-project/runner.py
@@ -53,8 +53,8 @@ class Runner:
         torch.backends.cudnn.deterministic = True
 
     def _build_model(self):
-        # self.model = FC(self.args, self.vocab)
-        self.model = RNN(self.args, self.vocab)
+        self.model = FC(self.args, self.vocab)
+        # self.model = RNN(self.args, self.vocab)
         # self.model = TextCNN(self.args, self.vocab)
 
         self.model.to(self.device)
